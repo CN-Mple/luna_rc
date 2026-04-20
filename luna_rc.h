158122a9ab88b358b895cda05142dfb6a51428ea
Purple Moon <2434717958@qq.com>
2026-04-20 14:19:58 +0800

    Add files via upload

---
+++ luna_rc.h
@@ -0,0 +1,68 @@
+/* luna_rc.h */
+#ifndef LUNA_RC_H
+#define LUNA_RC_H
+
+#include <stdint.h>
+
+#ifndef LUNA_ASSERT
+#include <assert.h>
+#define LUNA_ASSERT	assert
+#endif
+
+#ifndef LUNA_MALLOC
+#include <stdlib.h>
+#define LUNA_MALLOC	malloc
+#endif
+
+#ifndef LUNA_FREE
+#include <stdlib.h>
+#define LUNA_FREE	free
+#endif
+
+struct rc {
+	uint32_t count;
+	void (*destroy)(void *data);
+};
+
+void *luna_rc_acquire(size_t size, void (*destroy)(void*));
+void *luna_rc_refer(void *data);
+void luna_rc_release(void *data);
+
+#endif
+
+#define LUNA_RC_IMPLEMENTATION
+#ifdef LUNA_RC_IMPLEMENTATION
+
+void *luna_rc_acquire(size_t size, void (*destroy)(void*))
+{
+	LUNA_ASSERT(size > 0);
+	struct rc *_rc = LUNA_MALLOC(sizeof(struct rc) + size);
+	LUNA_ASSERT(_rc);
+	_rc->count = 1;
+	_rc->destroy = destroy;
+	return (_rc + 1);
+}
+
+void *luna_rc_refer(void *data)
+{
+	LUNA_ASSERT(data);
+	struct rc *_rc = (struct rc *)data - 1;
+	_rc->count += 1;
+	return (_rc + 1);
+}
+
+void luna_rc_release(void *data)
+{
+	LUNA_ASSERT(data);
+	struct rc *_rc = (struct rc *)data - 1;
+	if (_rc->count > 0) {
+		if (--_rc->count == 0) {
+			if (_rc->destroy) {
+				_rc->destroy(data);
+			}
+			LUNA_FREE(_rc);
+		}
+	}
+}
+
+#endif
